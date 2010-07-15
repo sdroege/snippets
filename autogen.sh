ad5b42d7cf21eb69726c29e8869e867d3dd6f8ed
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2010-07-15 18:22:18 +0200

    Add configure parameters to enable more compiler warnings and -Werror
    and use these parameters by default when running autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -13,4 +13,4 @@ mkdir -p m4
 autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
-$srcdir/configure --enable-maintainer-mode "$@"
+$srcdir/configure --enable-maintainer-mode --enable-more-warnings --enable-warnings-as-errors "$@"
