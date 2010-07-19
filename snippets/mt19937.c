8824166116f7b870c2b34881dd4b6742abc45f12
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2010-07-19 10:19:31 +0200

    Fix indention

--- snippets/mt19937.c
+++ snippets/mt19937.c
@@ -43,7 +43,7 @@
 
 #include <stdio.h>
 
-/* Period parameters */  
+/* Period parameters */
 #define M 397
 #define MATRIX_A 0x9908b0dfUL   /* constant vector a */
 #define UPPER_MASK 0x80000000UL /* most significant w-r bits */
@@ -51,63 +51,61 @@
 
 /* initializes mt[N] with a seed */
 static void
-mt19937_init (SnippetsRand *rand, uint32_t s)
+mt19937_init (SnippetsRand * rand, uint32_t s)
 {
-    uint32_t *mt = rand->mt;
-    unsigned int mti;
+  uint32_t *mt = rand->mt;
+  unsigned int mti;
 
-    mt[0]= s & 0xffffffffUL;
-    for (mti=1; mti<N; mti++) {
-        mt[mti] = 
-	    (1812433253UL * (mt[mti-1] ^ (mt[mti-1] >> 30)) + mti); 
-        /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
-        /* In the previous versions, MSBs of the seed affect   */
-        /* only MSBs of the array mt[].                        */
-        /* 2002/01/09 modified by Makoto Matsumoto             */
-        mt[mti] &= 0xffffffffUL;
-        /* for >32 bit machines */
-    }
-    rand->mti = mti;
+  mt[0] = s & 0xffffffffUL;
+  for (mti = 1; mti < N; mti++) {
+    mt[mti] = (1812433253UL * (mt[mti - 1] ^ (mt[mti - 1] >> 30)) + mti);
+    /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
+    /* In the previous versions, MSBs of the seed affect   */
+    /* only MSBs of the array mt[].                        */
+    /* 2002/01/09 modified by Makoto Matsumoto             */
+    mt[mti] &= 0xffffffffUL;
+    /* for >32 bit machines */
+  }
+  rand->mti = mti;
 }
 
 /* generates a random number on [0,0xffffffff]-interval */
 static uint32_t
 mt19937_genrand_uint32 (SnippetsRand * rand)
 {
-    static const uint32_t mag01[2]={0x0UL, MATRIX_A};
-    uint32_t y;
-    uint32_t *mt = rand->mt;
-    unsigned int mti = rand->mti;
+  static const uint32_t mag01[2] = { 0x0UL, MATRIX_A };
+  uint32_t y;
+  uint32_t *mt = rand->mt;
+  unsigned int mti = rand->mti;
 
-    /* mag01[x] = x * MATRIX_A  for x=0,1 */
+  /* mag01[x] = x * MATRIX_A  for x=0,1 */
 
-    if (mti >= N) { /* generate N words at one time */
-        int kk;
+  if (mti >= N) {               /* generate N words at one time */
+    int kk;
 
-        for (kk=0;kk<N-M;kk++) {
-            y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-            mt[kk] = mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1UL];
-        }
-        for (;kk<N-1;kk++) {
-            y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-            mt[kk] = mt[kk+(M-N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
-        }
-        y = (mt[N-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
-        mt[N-1] = mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1UL];
-
-        mti = 0;
+    for (kk = 0; kk < N - M; kk++) {
+      y = (mt[kk] & UPPER_MASK) | (mt[kk + 1] & LOWER_MASK);
+      mt[kk] = mt[kk + M] ^ (y >> 1) ^ mag01[y & 0x1UL];
     }
-  
-    y = mt[mti++];
+    for (; kk < N - 1; kk++) {
+      y = (mt[kk] & UPPER_MASK) | (mt[kk + 1] & LOWER_MASK);
+      mt[kk] = mt[kk + (M - N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
+    }
+    y = (mt[N - 1] & UPPER_MASK) | (mt[0] & LOWER_MASK);
+    mt[N - 1] = mt[M - 1] ^ (y >> 1) ^ mag01[y & 0x1UL];
 
-    /* Tempering */
-    y ^= (y >> 11);
-    y ^= (y << 7) & 0x9d2c5680UL;
-    y ^= (y << 15) & 0xefc60000UL;
-    y ^= (y >> 18);
+    mti = 0;
+  }
 
-    rand->mti = mti;
+  y = mt[mti++];
 
-    return y;
+  /* Tempering */
+  y ^= (y >> 11);
+  y ^= (y << 7) & 0x9d2c5680UL;
+  y ^= (y << 15) & 0xefc60000UL;
+  y ^= (y >> 18);
+
+  rand->mti = mti;
+
+  return y;
 }
-
